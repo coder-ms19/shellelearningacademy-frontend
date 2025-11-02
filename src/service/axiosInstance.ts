0111c7dc3ec00027835f085743786af9019a0e01
manish <manishkeer530@gmail.com>
2025-11-02 18:13:52 +0530

    render url added in axiosinstance

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL ="http://localhost:3000"
-// const BASE_URL ="https://shellentertenment-backend.onrender.com"
+// const BASE_URL ="http://localhost:3000"
+const BASE_URL ="https://shellentertenment-backend.onrender.com"
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api/v1`, // Set the base URL once here
